5706c01553561c8e2862eab2abe1ba0dd37a2314
Chelsea H <crileyprv@gmail.com>
2024-01-19 12:35:03 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,59 @@
+//Global Scope
+
+// Wait for the DOM to finish loading before executing the game code
+document.addEventListener("DOMContentLoaded", function() {
+    // Add event listeners to the button elements and execute code when a user clicks them
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+
+//Function Scope
 function runGame() {
-
+// Local Scope
 }
 
+//Function Scope
 function checkAnswer(){
-
+// Local Scope
 }
 
+//Function Scope
 function calculateCorrectAnswer() {
-
+// Local Scope
 }
 
+//Function Scope
 function incrementScore() {
-
+// Local Scope
 }
 
+//Function Scope
 function incrementWrongAnswer() {
-
+// Local Scope
 }
 
+//Function Scope
 function displayAdditionQuestion() {
-
+// Local Scope
 }
 
+//Function Scope
 function displaySubtractionQuestion() {
-
+// Local Scope
 }
 
+//Function Scope
 function displayMultiplicationQuestion() {
-    
+// Local Scope
 }
