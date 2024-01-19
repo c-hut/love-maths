f744e4eed654f48f5815f348e8745146a00b1336
Chelsea H <crileyprv@gmail.com>
2024-01-19 14:40:23 +0000

    Display addition questions to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,36 +2,57 @@
 
 //Global Scope
 
-// Wait for the DOM to finish loading before executing the game code
+// Waits for the DOM to finish loading before executing the game code
 document.addEventListener("DOMContentLoaded", function() {
-    // Add event listeners to the button elements and execute code when a user clicks them
+    // Adds event listeners to the button elements and executes code when a user clicks them
     let buttons = document.getElementsByTagName("button");
 
-    // Execute a loop whose values are sourced from an iterable object; in this case, it's an array of buttons
+    // Executes a loop whose values are sourced from an iterable object; in this case, it's an array of buttons
     for (let button of buttons) {
-        // Listen for a click, and execute the function once heard
+        // Listens for a click, and executes the function once heard
         button.addEventListener("click", function() {
             // If the Submit button is clicked...
             if (this.getAttribute("data-type") === "submit") {
-                //... send an alert to the user letting them know that they clicked the Submit button (visible at the top of the page)
+                //... sends an alert to the user letting them know that they clicked the Submit button (visible at the top of the page)
                 alert("You clicked Submit!");
               // If any of the other buttons are clicked...
             } else {
-                //... send an alert to the user letting them know which button they clicked...
+                // Lets the event listener know that it should run a game when one of the remaining buttons is clicked...
                 let gameType = this.getAttribute("data-type");
-                //... this is how the clicked button will be identified
-                alert(`You clicked ${gameType}`);
+                //... when the user clicks a button, the code for the corresponding game type will be executed
+                runGame(gameType);
             }
         })
     }
+    // The runGame function is passed to the event listener and the "addition" variable is passed along with it
+    runGame("addition");
+
 })
 
 
 //Function Scope
-function runGame() {
+
+/** 
+* This is the main game "loop"; it will be called when the script is first loaded
+* and after a user's answer has been processed
+*/
+
+// Side-note: docstrings (as above) will display upon hover if their accompanying function is called at a later point
+
+function runGame(gameType) {
 // Local Scope
+    // Creates two random numbers between 1 and 25, respectively
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    // If the addition button is clicked, two numbers (as per the above two lines of code) will be displayed with a '+' sign in the middle
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        // If there's an error, the code execution will stop...
+        alert(`Unknown game type: ${gameType}`);
+        // ... and it will throw an error
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 //Function Scope
@@ -55,8 +76,11 @@ function incrementWrongAnswer() {
 }
 
 //Function Scope
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 // Local Scope
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 //Function Scope
