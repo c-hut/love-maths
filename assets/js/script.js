8789b238fe5a0ed25c4cd054c33bf18e42103caf
Chelsea H <crileyprv@gmail.com>
2024-01-19 12:36:23 +0000

    Add explanatory comments for ease of understanding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// N.B. I've decided to add comments in order to better understand what each part of the code is doing
+
 //Global Scope
 
 // Wait for the DOM to finish loading before executing the game code
@@ -5,12 +7,19 @@ document.addEventListener("DOMContentLoaded", function() {
     // Add event listeners to the button elements and execute code when a user clicks them
     let buttons = document.getElementsByTagName("button");
 
+    // Execute a loop whose values are sourced from an iterable object; in this case, it's an array of buttons
     for (let button of buttons) {
+        // Listen for a click, and execute the function once heard
         button.addEventListener("click", function() {
+            // If the Submit button is clicked...
             if (this.getAttribute("data-type") === "submit") {
+                //... send an alert to the user letting them know that they clicked the Submit button (visible at the top of the page)
                 alert("You clicked Submit!");
+              // If any of the other buttons are clicked...
             } else {
+                //... send an alert to the user letting them know which button they clicked...
                 let gameType = this.getAttribute("data-type");
+                //... this is how the clicked button will be identified
                 alert(`You clicked ${gameType}`);
             }
         })
